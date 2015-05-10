95247041d10b47c024f9a53f18644c124efa8766
Derrick1024 <470413803@qq.com>
2015-05-10 21:38:52 +0800

    mpu6050_ok to usart

---
+++ delay/systick_delay.h
@@ -0,0 +1,9 @@
+#ifndef _DELAY__H_
+#define _DELAY__H_
+#include "stm32f10x.h" 
+
+void Delay_us(uint32_t n);
+void Delay_ms(uint32_t n);
+
+#endif
+
---
+++ usart/bsp_usart1.c
@@ -0,0 +1,77 @@
+
+#include "bsp_usart1.h"
+
+
+ /**
+  * @brief  USART1 GPIO 配置,工作模式配置。115200 8-N-1
+  * @param  无
+  * @retval 无
+  */
+void USART1_Config(void)
+{
+		GPIO_InitTypeDef GPIO_InitStructure;
+		USART_InitTypeDef USART_InitStructure;
+		NVIC_InitTypeDef NVIC_InitStructure;
+		
+		/* config USART1 clock */
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+		
+		/* USART1 GPIO config */
+		/* Configure USART1 Tx (PA.09) as alternate function push-pull */
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+		GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+		/* Configure USART1 Rx (PA.10) as input floating */
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+		GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+		
+			
+		/* USART1 mode config */
+		USART_InitStructure.USART_BaudRate = 2400;
+		USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+		USART_InitStructure.USART_StopBits = USART_StopBits_1;
+		USART_InitStructure.USART_Parity = USART_Parity_No ;
+		USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+		USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+		USART_Init(USART1, &USART_InitStructure); 
+		
+		USART_Cmd(USART1, ENABLE);
+		
+		//Usart1 NVIC 配置
+
+		NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+		NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+		NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=0 ;
+		NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;		//
+
+		NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
+		NVIC_Init(&NVIC_InitStructure);	//根据NVIC_InitStruct中指定的参数初始化外设NVIC寄存器USART1
+		
+		USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启中断
+}
+
+///重定向c库函数printf到USART1
+int fputc(int ch, FILE *f)
+{
+		/* 发送一个字节数据到USART1 */
+		USART_SendData(USART1, (uint8_t) ch);
+		
+		/* 等待发送完毕 */
+		while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);		
+	
+		return (ch);
+}
+
+///重定向c库函数scanf到USART1
+int fgetc(FILE *f)
+{
+		/* 等待串口1输入数据 */
+		while (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);
+
+		return (int)USART_ReceiveData(USART1);
+}
+/*********************************************END OF FILE**********************/
